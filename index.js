12a637c3c07a7a556a9c8679374f2b2041b6f971
sylvain121 <sylvain121@gmail.com>
2018-02-06 12:57:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const clim = require("./lib/tcpServer.js");
+
+module.exports = function(sails) {
+	return {
+		install: function() {
+	var clim = {
+		device: {
+			name: "clim_salon",
+			protocol: 'wifi',
+			service: 'airton',
+			identifier: 1
+
+		},
+		types: [
+			{
+				type: 'power',
+				sensor: false,
+				tag: "clim_salon",
+				category: 'switch',
+				min: 0,
+				max: 1
+
+			}
+
+		]
+
+	};
+	return gladys.device.create(device)
+
+
+		},
+		exec: function(params) {
+			switch(params) {
+				case "on":
+					clim.setOn24Hot();
+					break;
+				case "off":
+					clim.setOff();
+			}
+		},
+		setup: function() {
+			console.log(gladys.device.create());
+		}
+
+	};
+}
